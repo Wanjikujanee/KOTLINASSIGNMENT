9ccc71d30bd3243f99eff1e87396c44c31a564e3
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-02-17 21:15:27 +0300

    my kotlin work

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var studentName:String="Janemary wanjiku"
+    println(studentName)
+    var age =23
+    println(age)
+    var phoneNumber:String="0785456656"
+    println(phoneNumber)
+    var weight=45
+    println(weight)
+    val kenyan:Boolean=true
+    println(kenyan)
+}
